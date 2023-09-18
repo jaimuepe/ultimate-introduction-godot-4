1bee7e7c9c436c030021de2891ea54b0cc979283
Jaime Muela Pérez <jaimuepe@gmail.com>
2023-09-18 02:12:10 +0200

    camera, laser and grenades

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,1 +1,25 @@
 extends Node2D
+
+var laser_scene = preload("res://scenes/projectiles/laser.tscn")
+var grenade_scene = preload("res://scenes/projectiles/grenade.tscn")
+
+func _on_gate_player_entered_gate(_body):
+	print('player entered gate from level!')
+
+func _on_player_laser(pos: Vector2, dir: Vector2):
+	
+	var laser = laser_scene.instantiate()
+	laser.position = pos
+	laser.rotation = dir.angle()
+	laser.direction = dir
+	
+	$Projectiles.add_child(laser)
+
+func _on_player_grenade(pos: Vector2, dir: Vector2):
+	
+	var grenade = grenade_scene.instantiate() as RigidBody2D
+	grenade.position = pos
+	grenade.rotation = dir.angle()
+	grenade.linear_velocity = dir * grenade.speed
+	
+	$Projectiles.add_child(grenade)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
+signal laser(pos: Vector2, direction: Vector2)
+signal grenade(pos: Vector2, direction: Vector2)
+
 const speed: int = 500
+
+var can_laser: bool = true
+var can_grenade: bool = true
 
 func _ready():
 	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
@@ -11,10 +17,44 @@ func _process(_delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	move_and_slide()
+	
+	#rotate	
+	look_at(get_global_mouse_position())
 
-	# laser shooting input
-	if Input.is_action_pressed("primary action"):
-		print('shoot laser')
+	if can_laser && Input.is_action_pressed("primary action"):
+		shoot_laser()
 
-	if Input.is_action_pressed("secondary action"):
-		print('shoot grenade')
+	if can_grenade && Input.is_action_pressed("secondary action"):
+		shoot_grenade()
+
+func shoot_laser():
+	
+	can_laser = false
+	$LaserTimer.start()
+	
+	var markers = $LaserStartPositions.get_children()
+	var selected_marker = markers.pick_random()
+	
+	var pos = selected_marker.global_position
+	var dir = (get_global_mouse_position() - position).normalized()
+	
+	laser.emit(pos, dir)
+
+func shoot_grenade():
+	
+	can_grenade = false
+	$GrenadeTimer.start()
+	
+	var selected_marker = $LaserStartPositions.get_children()[1]
+	
+	var pos = selected_marker.global_position
+	var dir = (get_global_mouse_position() - position).normalized()
+	
+	grenade.emit(pos, dir)
+
+func _on_laser_timer_timeout():
+	can_laser = true
+
+
+func _on_grenade_timer_timeout():
+	can_grenade = true
